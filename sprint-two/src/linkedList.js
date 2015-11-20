e81e867f5cde4f6e950bdd06444c919318a795af
dakota-peel <koteman624@gmail.com>
2015-11-19 16:14:45 -0800

    Completed "linkedList"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      this.head = Node(value);
+      this.tail = this.head;
+    } else {
+      this.tail.next = Node(value);
+      this.tail = this.tail.next;
+    }      
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target) {
+    var result = false;
+    for (var key = list.head; key; key = key.next) {
+      if (target === key.value) {
+        result = true;
+      }
+
+    }
+    return result;
   };
 
   return list;
