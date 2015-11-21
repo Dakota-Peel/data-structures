1892bca6660f74823009feaf38ff03ec579a72e2
dakota-peel <koteman624@gmail.com>
2015-11-21 11:41:06 -0800

    Completed "treeAdv", but tweaking to return removeFromParent

---
+++ advanced/spec/treeAdvancedSpec.js
@@ -0,0 +1,69 @@
+describe('treeAdvanced', function() {
+  var tree;
+
+  beforeEach(function() {
+    tree = TreeAdv();
+  });
+
+  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+    expect(tree.addChild).to.be.a("function");
+    expect(tree.contains).to.be.a("function");
+    expect(tree.hasOwnProperty("value")).to.equal(true);
+  });
+
+  it('should add children to the tree', function() {
+    tree.addChild(5);
+    expect(tree.children[0].value).to.equal(5);
+  });
+
+  it('should return true for a value that the tree contains', function(){
+    tree.addChild(5);
+    expect(tree.contains(5)).to.equal(true);
+  });
+
+  it('should return false for a value that was not added', function(){
+    tree.addChild(5);
+    expect(tree.contains(6)).to.equal(false);
+  });
+
+  it('should be able to add children to a tree\'s child', function() {
+    tree.addChild(5);
+    tree.children[0].addChild(6);
+    expect(tree.children[0].children[0].value).to.equal(6);
+  });
+
+  it('should correctly detect nested children', function(){
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    expect(tree.contains(7)).to.equal(true);
+    expect(tree.contains(8)).to.equal(true);
+  });
+
+  it ('should refer to the parent node or null when there is no node', function() {
+    //tests
+    tree.addChild(2);
+    tree.addChild(4);
+    tree.addChild(6);
+    tree.children[0].addChild(5);
+    tree.children[1].addChild(10);
+    tree.children[2].addChild(15);
+    expect(tree.children[1].children[0].parent.value).to.equal(4);
+    expect(tree.parent).to.equal(null);
+  });
+
+  it('.removeFromParent() method should disassociate the tree with its parent (in both directions)', function() {
+    tree.addChild(2);
+    tree.addChild(4);
+    tree.addChild(6);
+    tree.children[0].addChild(5);
+    tree.children[1].addChild(10);
+    tree.children[2].addChild(15);
+    expect(tree.children[1].removeFromParent()).to.equal(4);
+    expect(tree.children[1].value).to.equal(6);
+    // expect(tree.parent).to.equal(null);
+  })
+
+
+});
---
+++ advanced/src/treeAdvanced.js
@@ -0,0 +1,45 @@
+var TreeAdv = function(value) {
+  var newTree = {};
+  newTree.value = value;
+
+  // your code here
+  newTree.children = [];  // fix me
+  newTree.parent = null;
+  _.extend(newTree, treeMethods);
+  return newTree;
+};
+
+var treeMethods = {};
+
+treeMethods.addChild = function(value) {
+  // your code here
+  this.children.push(TreeAdv(value));
+  this.children[this.children.length - 1].parent = this;
+};
+
+treeMethods.contains = function(target) {
+  var result = false;
+  var helper = function(node){
+    if (node.value === target) {
+      result = true;
+    } else {
+      _.each(node.children, function(item){
+        helper(item);
+      });
+    }
+  }
+  helper(this);
+  return result;
+};
+
+treeMethods.removeFromParent = function () {
+  // this.parent.children = null;
+  var index = this.parent.children.indexOf(this);
+  var temp = this.parent.children.splice(index, 1);
+  return temp.value;
+};
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
